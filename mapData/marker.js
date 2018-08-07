aa8ce59ab0245736a6f08778058bf3e701b1b98d
CaiShengji <cai.shengji@nndeal.com>
2018-08-07 10:09:42 +0800

    add

---
+++ mapData/marker.js
@@ -0,0 +1,22 @@
+var provinces = [{
+    "name": "上海",
+    "center": "121.4623250000,31.2278380000",
+    "docNumber": 33,
+    "id":'021_1',
+    "cityNum":'021',
+    "subDistricts": []
+},{
+    "name": "北京",
+    "center": "116.5216100000,39.9139700000",
+    "docNumber": 17,
+    "id":'021_2',
+    "cityNum":'010',
+    "subDistricts": []
+},{
+    "name": "武汉",
+    "center": "114.3450379372,30.5311596643",
+    "docNumber": 7,
+    "id":'021_3',
+    "cityNum":'027',
+    "subDistricts": []
+}];
